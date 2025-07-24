6a890ba35880da7235824d98c50cc1533d7c4f75
Yannick Erdmann <yannick.erdmann@live.fr>
2025-07-24 18:31:37 +0200

    Fix header links to use relative paths

--- includes/header.php
+++ includes/header.php
@@ -11,20 +11,20 @@ require_once __DIR__.'/auth.php';
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/index.php">Budget</a>
+    <a class="navbar-brand" href="index.php">Budget</a>
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item"><a class="nav-link" href="/transactions/list.php">Transactions</a></li>
-        <li class="nav-item"><a class="nav-link" href="/categories/list.php">Catégories</a></li>
-        <li class="nav-item"><a class="nav-link" href="/subcategories/list.php">Sous-catégories</a></li>
-        <li class="nav-item"><a class="nav-link" href="/payment_modes/list.php">Modes de paiement</a></li>
-        <li class="nav-item"><a class="nav-link" href="/accounts/list.php">Comptes</a></li>
+        <li class="nav-item"><a class="nav-link" href="transactions/list.php">Transactions</a></li>
+        <li class="nav-item"><a class="nav-link" href="categories/list.php">Catégories</a></li>
+        <li class="nav-item"><a class="nav-link" href="subcategories/list.php">Sous-catégories</a></li>
+        <li class="nav-item"><a class="nav-link" href="payment_modes/list.php">Modes de paiement</a></li>
+        <li class="nav-item"><a class="nav-link" href="accounts/list.php">Comptes</a></li>
       </ul>
       <ul class="navbar-nav">
         <?php if (current_user_id()): ?>
-            <li class="nav-item"><a class="nav-link" href="/logout.php">Se déconnecter</a></li>
+            <li class="nav-item"><a class="nav-link" href="logout.php">Se déconnecter</a></li>
         <?php else: ?>
-            <li class="nav-item"><a class="nav-link" href="/login.php">Se connecter</a></li>
+            <li class="nav-item"><a class="nav-link" href="login.php">Se connecter</a></li>
         <?php endif; ?>
       </ul>
     </div>
